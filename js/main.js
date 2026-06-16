3e24075c90d6433419477e8b1955d06da46f9503
Konstantin <168019285+3069603@users.noreply.github.com>
2026-06-16 03:48:11 +0300

    Reduce preloader removal timeout from 1500ms to 500ms

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
   const preloader = document.getElementById("preloader");
   preloader.classList.add("fade-out");
-  setTimeout(() => preloader.remove(), 1500);
+  setTimeout(() => preloader.remove(), 500);
 });
